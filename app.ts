fa5f9e8fe46d3d5ac0471f04da785a8d3147097e
Guksu <yung3152@gmail.com>
2023-04-10 21:37:56 +0900

    [2023-04-10]
    1.  이미지 업로드 로직 추가

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import helmet from "helmet";
 import compression from "compression";
+import multer from "multer";
+import path from "path";
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 
@@ -26,6 +28,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
+app.use("/uploads", express.static("uploads"));
 
 const io = new Server(httpServer, {
   path: "/socket/chat",
@@ -44,6 +47,28 @@ io.on("connection", (socket: Socket) => {
 
   socket.on("disconnect", () => {
     console.log(`유저나감 : ${new Date()}  유저아이디:${socket.id}`);
+  });
+});
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/chat");
+    },
+    filename: function (req, file, cb) {
+      const ext = path.extname(file.originalname);
+      cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
+    },
+  }),
+});
+
+app.post("/upload", upload.single("chatImage"), (req: Request, res: Response) => {
+  const imagePath: string = req.file ? `uploads/chat/${req.file?.filename}` : "";
+
+  res.status(200).send({
+    status: 200,
+    message: "ok",
+    data: { chatImg: imagePath },
   });
 });
 
