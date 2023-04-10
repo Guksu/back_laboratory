bb754a7eb4801a1b2ea5c641555c6eacd59cc60b
Guksu <yung3152@daum.net>
2023-04-10 22:00:51 +0900

    노트북 커밋확인용

--- app.ts
+++ app.ts
@@ -28,7 +28,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(compression());
-// app.use("/uploads", express.static("uploads"));
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
 const io = new Server(httpServer, {
